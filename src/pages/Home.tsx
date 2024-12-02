9451d27de9ec0354c09b802b9345b3d6015f1acb
hossein ghaniamti <hosein.ghanimati1398@gmail.com>
2024-12-02 19:42:59 +0330

    Develop Withot Redux

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,24 @@
+import { useCallback, useState } from "react"
 import "../assets/styles/counter.css"
 
 const Home = () => {
+  const [count, setCount] = useState(0)
+
+  const reset = useCallback(() => setCount(0), [])
+  const increment = useCallback(() => setCount(count => count + 1), [])
+  const decrement = useCallback(() => setCount(count => count - 1), [])
+
   return (
     <div className="container">
-      <h1 id="num">{0}</h1>
+      <h1 id="num">{count}</h1>
       <div className="btns">
-        <button className="dec" onClick={() => {}}>
+        <button className="dec" onClick={decrement}>
           <i className="fas fa-minus"></i>
         </button>
-        <button className="reset" onClick={() => {}}>
+        <button className="reset" onClick={reset}>
           <i className="fas fa-redo"></i>
         </button>
-        <button className="inc" onClick={() => {}}>
+        <button className="inc" onClick={increment}>
           <i className="fas fa-plus"></i>
         </button>
       </div>
